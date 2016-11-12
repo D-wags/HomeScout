5b4ad2250ead0b69c762e17a0b9ecccae724412e
KevinAngeles <kevin.angeles@outlook.com>
2016-11-12 14:45:02 -0600

    initial structure

---
+++ migrations/20161112200043-create-zip-stats.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('ZipStats', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      zip: {
+        type: Sequelize.CHAR
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('ZipStats');
+  }
+};
---
+++ models/zipstats.js
@@ -0,0 +1,67 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  	var ZipStats = sequelize.define('ZipStats', {
+		zip: {
+			type: DataTypes.CHAR(5),
+			unique: true,
+			allowNull: false,
+			primaryKey: true
+		},
+		percent_below_pov: {
+		  type: DataTypes.DECIMAL(19,4),
+		  allowNull: true,
+		},
+		median_income: {
+		  type: DataTypes.DECIMAL(19,4),
+		  allowNull: true
+		},
+		percent_unemployment: {
+		  type: DataTypes.DECIMAL(19,4),
+		  allowNull: true
+		},
+		percent_family_homes: {
+		  type: DataTypes.DECIMAL(19,4),
+		  allowNull: true
+		},
+		median_rent: {
+		  type: DataTypes.DECIMAL(19,4),
+		  allowNull: true
+		},
+		median_home_value: {
+		  type: DataTypes.DECIMAL(19,4),
+		  allowNull: true
+		},
+		percent_housing_growth: {
+		  type: DataTypes.DECIMAL(19,4),
+		  allowNull: true
+		},
+		percent_change_property_value: {
+		  type: DataTypes.DECIMAL(19,4),
+		  allowNull: true
+		},
+		percent_change_median_rent: {
+		  type: DataTypes.DECIMAL(19,4),
+		  allowNull: true
+		},
+		percent_home_close_to_bus_route: {
+		  type: DataTypes.DECIMAL(19,4),
+		  allowNull: true
+		},
+		annual_number_crimes: { 
+		  type: DataTypes.INTEGER(6),
+		  allowNull: true
+		},
+		google_fiber: {
+		  type: DataTypes.CHAR(1),
+		  allowNull: true
+		}
+	},
+	{
+		classMethods: {
+			associate: function(models) {
+			// associations can be defined here
+			}
+		}
+	});
+	return ZipStats;
+};
